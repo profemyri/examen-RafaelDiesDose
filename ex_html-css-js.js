60c634edb925daa52206f9c439c2687dcee2d946
RafaelDiesDose <ralccar479@g.educaand.es>
2025-03-17 19:29:47 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -5,30 +5,44 @@ let direccion=document.getElementById.trim(("direccion"));
 let telefono=document.getElementById.trim(("telefono"));
 let email=document.getElementById.trim(("email"));
 
-formulario.onsubmit(){
+formulario.onsubmit();{
 
-    if(nombre.value.trim().===" "){
+    if(nombre.value.trim()===" "){
         alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Nombre)");
-        return;
+        return false;
     };
 
     if(direccion.value.trim().length<15){
         alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Dirección)");
-        return;
+        return false;
     };
 
     if(isNaN(telefono)){
         alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Teléfono)");
-        return;
+        return false;
     };
 
     if(telefono.value.trim().length<9){
         alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Teléfono)");
-        return;
+        return false;
     };
 
     if(!email.contains("@")){
         alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Email)");
-        return;
+        return false;
     };
-}
+
+};
+
+let caracteres=document.getElementById.trim(("caracteres"));
+
+caracteres.oninput;{
+    let contador=document.getElementById.trim(("cont"));
+    contador.textContent=caracteres.value.length;
+};
+
+let boton=document.getElementById("envio");
+
+boton.onmouseover;{
+    background-color: #ffffff;
+};
