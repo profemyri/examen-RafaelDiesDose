49f32eee63c89374898b841df780d4602c558772
RafaelDiesDose <ralccar479@g.educaand.es>
2025-03-17 18:25:04 +0100

    .

--- ex_html-css-js.js
+++ ex_html-css-js.js
@@ -1,1 +1,24 @@
-//Escribe aquí tu código JavaScript
+let formulario=document.getElementById(formulario);
+
+let nombre=document.getElementById.trim(("nombre"));
+let direccion=document.getElementById.trim(("direccion"));
+let telefono=document.getElementById.trim(("telefono"));
+let email=document.getElementById.trim(("email"));
+
+formulario.onsubmit(){
+    if(nombre.value===""){
+        alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Nombre)");
+    }
+    if(direccion.value.length<15){
+        alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Dirección)");
+    }
+    if(isNaN(telefono)){
+        alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Teléfono)");
+    }
+    if(telefono.value.length<9){
+        alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Teléfono)");
+    }
+    if(!contains.email("@")){
+        alert("Revisa el formulario: algunos campos son incorrectos o están incompletos. (Email)");
+    }
+};
